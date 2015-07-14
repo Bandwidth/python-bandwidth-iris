f05e89617b4802e520fb44572483d03890c22693
daniel bavrin <daniel.bavrin@gmail.com>
2015-07-14 16:13:54 +0300

    added lidbs complete

--- iris_sdk/models/data/lidbs.py
+++ iris_sdk/models/data/lidbs.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 from iris_sdk.models.base_resource import BaseData, BaseResourceList
-from iris_sdk.models.data.links import Links
 from iris_sdk.models.maps.lidbs import LidbsMap
 from iris_sdk.models.lidb import Lidb
 
 class LidbsData(LidbsMap, BaseData):
 
     def __init__(self, parent=None):
-        self.lidb_tn_groups = BaseResourceList(Lidb, parent)
+        self.order_id_user_id_date = BaseResourceList(Lidb, parent)
