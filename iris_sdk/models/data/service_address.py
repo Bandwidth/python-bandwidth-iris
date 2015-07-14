c535e013dbb6a02e8ec37bef55de1132811aab49
daniel bavrin <daniel.bavrin@gmail.com>
2015-07-14 17:55:33 +0300

    added dldas

--- iris_sdk/models/data/service_address.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from iris_sdk.models.base_resource import BaseData
-from iris_sdk.models.maps.address import AddressMap
-
-class ServiceAddress(AddressMap, BaseData):
-    pass
