178edfc1299fab4805ac72b3df05dae4005fd10a
daniel bavrin <daniel.bavrin@gmail.com>
2015-07-14 20:59:25 +0300

    added siteid/orders and portins

--- iris_sdk/models/site.py
+++ iris_sdk/models/site.py
@@ -7,6 +7,8 @@ from iris_sdk.models.base_resource import BaseResource, BaseResourceList
 from iris_sdk.models.data.site import SiteData
 from iris_sdk.models.sip_peers import SipPeers
 from iris_sdk.models.site_totaltns import SiteTotaltns
+from iris_sdk.models.orders import Orders
+
 
 XPATH_SITE = "/{}"
 
@@ -15,6 +17,14 @@ class Site(BaseResource, SiteData):
     """Account site"""
 
     _xpath = XPATH_SITE
+
+    @property
+    def orders(self):
+        return self._orders
+
+    @property
+    def portins(self):
+        return self._portins
 
     @property
     def sip_peers(self):
@@ -27,6 +37,8 @@ class Site(BaseResource, SiteData):
     def __init__(self, parent=None, client=None):
         super().__init__(parent, client)
         SiteData.__init__(self)
+        self._orders = Orders(self, client)
+        self._portins = PortIns(self, client)
         self._sip_peers = SipPeers(self, client)
         self._totaltns = SiteTotaltns(self, client)
 
