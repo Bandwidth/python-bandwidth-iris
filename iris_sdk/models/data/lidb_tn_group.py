104ee1f18bbab2cc809196695f0ccf59a9c6d99b
daniel bavrin <daniel.bavrin@gmail.com>
2015-07-14 19:58:47 +0300

    removed class subscriber information -- it's a string in documentation

--- iris_sdk/models/data/lidb_tn_group.py
+++ iris_sdk/models/data/lidb_tn_group.py
@@ -3,10 +3,8 @@
 from iris_sdk.models.base_resource import BaseData
 from iris_sdk.models.maps.lidb_tn_group import LidbTnGroupMap
 from iris_sdk.models.data.telephone_number_list import TelephoneNumberList
-from iris_sdk.models.data.subscriber_information import SubscriberInformation
-
+f
 class LidbTnGroupData(LidbTnGroupMap, BaseData):
 
     def __init__(self):
         self.telephone_numbers = TelephoneNumberList()
-        self.subscriber_information = SubscriberInformation()
